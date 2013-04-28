f877d5c1ff6e9da3a7e69ead255fc14626d2a562
Calvin De Lima <calvindlm@gmail.com>
2013-04-28 19:10:32 -0400

    Modified range function and added __init__.py

--- redis_timeline.py
+++ redis_timeline.py
@@ -8,76 +8,74 @@ from __future__ import absolute_import
 from redis import StrictRedis
 
 class RedisTimeline(object):
-	_redis = None
+    _redis = None
 
-	def __init__(self, **kwargs):
-		"""
-		Creates Redis object with key ``timelines:<id>``.
-		"""
+    def __init__(self, **kwargs):
+        """
+        Creates Redis object with key ``timelines:<id>``.
+        """
 
-		name = kwargs.pop('name')
-		prefix = kwargs.pop('prefix', "timelines")
-		sep = kwargs.pop('sep', ":")
-		
-		self.name = "%s%s%s" % (prefix, sep, name)
+        name = kwargs.pop('name')
+        prefix = kwargs.pop('prefix', "timelines")
+        sep = kwargs.pop('sep', ":")
+        
+        self.name = "%s%s%s" % (prefix, sep, name)
 
-		# Default to uncapped length
-		self.max_length = kwargs.pop("max_length", 0)
+        # Default to uncapped length
+        self.max_length = kwargs.pop("max_length", 0)
 
-		if self._redis is None:
-			url = kwargs.get('url', None)
+        if self._redis is None:
+            url = kwargs.get('url', None)
 
-			if url is not None:
-				self._redis = StrictRedis().from_url(url)
-			else:
-				self._redis = StrictRedis(**kwargs)
+            if url is not None:
+                self._redis = StrictRedis().from_url(url)
+            else:
+                self._redis = StrictRedis(**kwargs)
 
-	def push(self, value):
-		"""
-		Adds the value to the front of the list and trim if the 
-		max length != 0. Returns the value.
-		"""
+    def push(self, value):
+        """
+        Adds the value to the front of the list and trim if the 
+        max length != 0. Returns the value.
+        """
 
-		self._redis.lpush(self.name, value)
+        self._redis.lpush(self.name, value)
 
-		if self.max_length > 0:
-			self._redis.ltrim(self.name, 0, self.max_length-1)
+        if self.max_length > 0:
+            self._redis.ltrim(self.name, 0, self.max_length-1)
 
-		return value
+        return value
 
-	def range(self, count=0, **kwargs):
-		"""
-		Returns a range values from the front of the list to the limit.
-		"""
+    def range(self, count=0, **kwargs):
+        """
+        Returns a range values from the front of the list to the limit.
+        """
 
-		redis = self._redis
+        if count is 0:
+            # No count, check for offset/limit
+            offset = kwargs.get('offset', 0)
+            limit = kwargs.get('limit', -1)
+        else:
+            # If there's a count, default to offset 0
+            offset = 0
+            limit = count
 
-		if count is 0:
-			offset = kwargs.get('offset', 0)
-			limit = kwargs.get('limit', -1)
-		else:
-			offset = 0
-			limit = count
+        if limit is 1: 
+            # Get range of length 1 using LRANGE <timeline> <limit> <limit>
+            limit = offset
+        elif limit > 0:
+            limit = offset+limit - 1
 
-		if limit is 1: 
-			# Get `range` of length 1 using 
-			# LRANGE <timeline> <limit> <limit>
-			limit = offset
-		else:
-			# Regular n-1 rules
-			limit = limit - 1
+        return self._redis.lrange(self.name, offset, limit)
 
-		return redis.lrange(self.name, offset, offset+limit)
+    def count(self):
+        """
+        Uses Redis LLEN to get the length of the timeline.
+        """
+        return self._redis.llen(self.name)
 
-	def count(self):
-		"""
-		Uses Redis LLEN to get the length of the timeline.
-		"""
-		return self._redis.llen(self.name)
-
-	@property
-	def length(self):
-		"""
-		Access the length (count) as a property.
-		"""
-		return self.count()
+    @property
+    def length(self):
+        """
+        Access the length (count) as a property.
+        """
+        return self.count()
